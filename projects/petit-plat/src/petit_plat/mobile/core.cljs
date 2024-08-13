aa19a571f564b55d10b5350bcb0047d70b08ef5d
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-08-14 00:54:02 +0200

    chore: first commit

--- projects/petit-plat/src/petit_plat/mobile/core.cljs
+++ projects/petit-plat/src/petit_plat/mobile/core.cljs
@@ -2,18 +2,36 @@
   (:require
    ["expo" :as ex]
    ["react-native" :as rn]
+   ["@react-navigation/native" :refer [NavigationContainer]]
+   ["@react-navigation/stack" :refer [createStackNavigator]]
    [shadow.expo :as expo]
    [reagent.core :as r]
    [account.interface :refer [hello]]))
 
+(def stack (createStackNavigator))
 
-(defn root []
-  [:> rn/Text {:style {:font-size 30}} (hello)])
+(defn home-screen []
+  (fn []
+    [:> rn/View
+     [:> rn/Text "Home Screen"]]))  ; Use a string instead of a keyword
+
+(defn details-screen []
+  (fn []
+    [:> rn/View
+     [:> rn/Text "Details Screen"]]))
+
+(defn stack-navigator []
+  (let [StackNavigator (.-Navigator stack)
+        StackScreen (.-Screen stack)]
+    [:> NavigationContainer
+     [:> StackNavigator
+      [:> StackScreen {:name "Home" :component home-screen}]
+      [:> StackScreen {:name "Details" :component details-screen}]]]))
 
 (defn start
   {:dev/after-load true}
   []
-  (expo/render-root (r/as-element  [root])))
+  (expo/render-root (r/as-element [stack-navigator])))
 
 (comment
   (start))
