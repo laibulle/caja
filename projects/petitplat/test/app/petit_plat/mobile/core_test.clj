1e40c0f6de18f0ab991dac40b3681457bee8ec50
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-08-24 19:06:07 +0200

    chore: first commit

--- projects/petitplat/test/app/petit_plat/mobile/core_test.clj
+++ projects/petitplat/test/app/petit_plat/mobile/core_test.clj
@@ -1,6 +1,6 @@
-(ns app.petit-plat.mobile.core-test
+(ns app.petitplat.mobile.core-test
   (:require [clojure.test :as test :refer :all]
-            [app.petit-plat.mobile.core :as core]))
+            [app.petitplat.mobile.core :as core]))
 
 (deftest dummy-test
   (is (= 1 1)))
