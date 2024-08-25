6e16ebf0108fb0c55ce60430972d000aef4f40ca
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-08-25 19:44:27 +0200

    chore: first commit

--- projects/petitplat/test/petitplat/mobile/infrastructure/local_storage_test.cljs
+++ projects/petitplat/test/petitplat/mobile/infrastructure/local_storage_test.cljs
@@ -2,44 +2,31 @@
   (:require
    [cljs.test :refer-macros [deftest is async run-tests]]
    [petitplat.mobile.infrastructure.local-storage :as ls]
-   ["@react-native-async-storage/async-storage" :as mock-async-storage]
-   [reagent.core :as r]))
-
-(defn mock-set-item [key value]
-  (reset! mock-async-storage {key value}))
-
-(defn mock-get-item [key]
-  (get @mock-async-storage key))
+   ["@react-native-async-storage/async-storage" :as mock-async-storage]))
 
 (deftest test-set-item
   (async done
-    ;; Create a mock async-storage object
-         (with-redefs [ls/async-storage (r/atom {})]
-      ;; Mock setItem function
-           (with-redefs [mock-async-storage/setItem (fn [key value] (mock-set-item key value))]
-        ;; Test
-             (let [key "test-key"
-                   value {:foo "bar"}]
-               (ls/set-item key value)
-               (is (= (get @mock-async-storage key) (.stringify js/JSON (clj->js value))))
-               (done))))))
+    ;; Mock setItem function
+         (with-redefs [mock-async-storage/setItem (fn [key value]
+                                                    (reset! (atom {}) {key value}))]
+      ;; Test
+           (let [key "test-key"
+                 value {:foo "bar"}]
+             (ls/set-item key value)
+             (is (= (get @mock-async-storage key) (.stringify js/JSON (clj->js value))))
+             (done)))))
 
 (deftest test-get-item
   (async done
-    ;; Create a mock async-storage object
-         (with-redefs [ls/async-storage (r/atom {})]
-      ;; Mock getItem function
-           (with-redefs [mock-async-storage/getItem (fn [key] (mock-get-item key))]
-        ;; Test
-             (let [key "test-key"
-                   value {:foo "bar"}]
-          ;; Set an item manually in the mock storage
-               (reset! mock-async-storage {key (.stringify js/JSON (clj->js value))})
-          ;; Test get-item
-               (-> (ls/get-item key)
-                   (.then (fn [result]
-                            (is (= result value))
-                            (done)))))))))
+    ;; Mock getItem function
+         (with-redefs [mock-async-storage/getItem (fn [key]
+                                                    (js/Promise.resolve (.stringify js/JSON (clj->js {:foo "bar"}))))]
+      ;; Test
+           (let [key "test-key"]
+             (-> (ls/get-item key)
+                 (.then (fn [result]
+                          (is (= result {:foo "bar"}))
+                          (done))))))))
 
 (comment
   (run-tests))
