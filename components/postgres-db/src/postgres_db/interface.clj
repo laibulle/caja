afdc1293b362590f2a8d15c97f6c82162c3eaafa
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-01 04:21:26 +0200

    chore: first commit

--- components/postgres-db/src/postgres_db/interface.clj
+++ components/postgres-db/src/postgres_db/interface.clj
@@ -1,32 +1,26 @@
 (ns postgres-db.interface
-  (:require  [next.jdbc :as jdbc]
-             [next.jdbc.connection :as conn])
-  (:import (com.mchange.v2.c3p0 ComboPooledDataSource PooledDataSource)))
+  (:require  [next.jdbc :as jdbc]))
 
-(def db-spec
-  {:jdbcUrl "jdbc:postgresql://localhost:5432/yourdb"  ;; Replace with your actual DB URL
-   :user "yourusername"                               ;; Replace with your actual DB user
-   :password "yourpassword"                           ;; Replace with your actual DB password
-   ;; C3P0 specific settings
-   :connectionPoolDataSource (doto (com.mchange.v2.c3p0.ComboPooledDataSource.)
-                               (.setDriverClass "org.postgresql.Driver")
-                               (.setJdbcUrl "jdbc:postgresql://localhost:5432/yourdb")
-                               (.setUser "yourusername")
-                               (.setPassword "yourpassword")
-                               (.setMinPoolSize 5)
-                               (.setMaxPoolSize 20))})
+;; Define the connection pool using c3p0
+(defn create-datasource []
+  (doto (com.mchange.v2.c3p0.ComboPooledDataSource.)
+    (.setDriverClass "org.postgresql.Driver")
+    (.setJdbcUrl "jdbc:postgresql://localhost:5437/petitplat_dev") ;; Update with your actual database URL
+    (.setUser "postgres")                               ;; Update with your actual database username
+    (.setPassword "postgres")                           ;; Update with your actual database password
+    (.setMinPoolSize 5)
+    (.setMaxPoolSize 20)))
 
-(def datasource (conn/->pool ComboPooledDataSource db-spec))
-
-(defn get-connection []
-  (jdbc/get-datasource db-spec))
+(def datasource (atom nil))
 
 (defn fetch-data []
-  (jdbc/execute! (get-connection) ["SELECT * FROM your_table LIMIT 10"]))
+  (jdbc/execute! @datasource ["SELECT * FROM your_table LIMIT 10"]))
 
-
+(defn init []
+  (reset! datasource (create-datasource)))
 
 (comment
+  (init)
   (fetch-data)
   ;(init {:dbtype "postgres" :dbname "thedb" :username "dbuser" :password "secret"
    ;      :dataSourceProperties {:socketTimeout 30}})
