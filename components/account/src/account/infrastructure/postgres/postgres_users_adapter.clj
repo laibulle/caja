cf7f7bd57f69c37bf17f43842e043486620ef4bc
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-01 03:48:41 +0200

    chore: first commit

--- components/account/src/account/infrastructure/postgres/postgres_users_adapter.clj
+++ components/account/src/account/infrastructure/postgres/postgres_users_adapter.clj
@@ -1,3 +1,11 @@
-(ns account.infrastructure.postgres.postgres-users-adapter)
+(ns account.infrastructure.postgres.postgres-users-adapter
+  (:require [honey.sql :as sql]))
 
 (defn hello [])
+
+(defn get-user-by-email [email]
+  (-> {:select [:a :b :c]
+       :from   [:foo]
+       :where  [:= :foo.a "baz"]}
+      (sql/format)
+      (jdbc/execute! conn)))
