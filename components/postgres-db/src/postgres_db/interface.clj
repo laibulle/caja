0d9487b53bad365051449ff093df510cafd6fe03
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-01 04:09:41 +0200

    chore: first commit

--- components/postgres-db/src/postgres_db/interface.clj
+++ components/postgres-db/src/postgres_db/interface.clj
@@ -1,1 +1,33 @@
-(ns postgres-db.interface)
+(ns postgres-db.interface
+  (:require  [next.jdbc :as jdbc]
+             [next.jdbc.connection :as conn])
+  (:import (com.mchange.v2.c3p0 ComboPooledDataSource PooledDataSource)))
+
+(def db-spec
+  {:jdbcUrl "jdbc:postgresql://localhost:5432/yourdb"  ;; Replace with your actual DB URL
+   :user "yourusername"                               ;; Replace with your actual DB user
+   :password "yourpassword"                           ;; Replace with your actual DB password
+   ;; C3P0 specific settings
+   :connectionPoolDataSource (doto (com.mchange.v2.c3p0.ComboPooledDataSource.)
+                               (.setDriverClass "org.postgresql.Driver")
+                               (.setJdbcUrl "jdbc:postgresql://localhost:5432/yourdb")
+                               (.setUser "yourusername")
+                               (.setPassword "yourpassword")
+                               (.setMinPoolSize 5)
+                               (.setMaxPoolSize 20))})
+
+(def datasource (conn/->pool ComboPooledDataSource db-spec))
+
+(defn get-connection []
+  (jdbc/get-datasource db-spec))
+
+(defn fetch-data []
+  (jdbc/execute! (get-connection) ["SELECT * FROM your_table LIMIT 10"]))
+
+
+
+(comment
+  (fetch-data)
+  ;(init {:dbtype "postgres" :dbname "thedb" :username "dbuser" :password "secret"
+   ;      :dataSourceProperties {:socketTimeout 30}})
+  )
