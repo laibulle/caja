e6ec0288d9ff1ca9f94e45f1dbcbffdb2d7bc497
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-01 04:23:00 +0200

    chore: first commit

--- components/postgres-db/src/postgres_db/interface.clj
+++ components/postgres-db/src/postgres_db/interface.clj
@@ -2,12 +2,12 @@
   (:require  [next.jdbc :as jdbc]))
 
 ;; Define the connection pool using c3p0
-(defn create-datasource []
+(defn create-datasource [config]
   (doto (com.mchange.v2.c3p0.ComboPooledDataSource.)
     (.setDriverClass "org.postgresql.Driver")
-    (.setJdbcUrl "jdbc:postgresql://localhost:5437/petitplat_dev") ;; Update with your actual database URL
-    (.setUser "postgres")                               ;; Update with your actual database username
-    (.setPassword "postgres")                           ;; Update with your actual database password
+    (.setJdbcUrl (:url config)) ;; Update with your actual database URL
+    (.setUser (:user config))                               ;; Update with your actual database username
+    (.setPassword (:password config))                           ;; Update with your actual database password
     (.setMinPoolSize 5)
     (.setMaxPoolSize 20)))
 
@@ -16,12 +16,9 @@
 (defn fetch-data []
   (jdbc/execute! @datasource ["SELECT * FROM your_table LIMIT 10"]))
 
-(defn init []
-  (reset! datasource (create-datasource)))
+(defn init [config]
+  (reset! datasource (create-datasource config)))
 
 (comment
-  (init)
-  (fetch-data)
-  ;(init {:dbtype "postgres" :dbname "thedb" :username "dbuser" :password "secret"
-   ;      :dataSourceProperties {:socketTimeout 30}})
-  )
+  (init {:url "jdbc:postgresql://localhost:5437/petitplat_dev" :user "postgres" :password "postgres"})
+  (fetch-data))
