34ea8c27fe8cdc0c4f17ba741430dc7628202275
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-02 04:36:16 +0200

    chore: first commit

--- components/account/dev/user.clj
+++ components/account/dev/user.clj
@@ -1,11 +1,19 @@
 (ns user
   (:require
    [postgres-db.interface :as pi]
-   [account.use-cases.register-user-in-db-use-case :as ru]))
+   [account.use-cases.register-user-in-db-use-case :as ru]
+   [ragtime.next-jdbc :as jdbc]
+   [ragtime.repl :as repl]))
 
 (defn init []
-  (pi/init {:url "jdbc:postgresql://localhost:5437/petitplat_dev" :user "postgres" :password "postgres"}))
+  (pi/init {:url "jdbc:postgresql://localhost:5437/petitplat_dev?user=postgres&password=postgres"}))
+
+(def config
+  {:datastore  (jdbc/sql-database {:connection-uri "jdbc:postgresql://localhost:5437/petitplat_dev?user=postgres&password=postgres"})
+   :migrations (jdbc/load-resources "migrations")})
 
 (comment
   (init)
+
+  (repl/migrate config)
   (ru/execute {:name "John Doe" :email "j@gmail.com" :password "Noirfnefwerf#mopgmtrogmroptgm"}))
