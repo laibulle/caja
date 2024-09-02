afdd02d927b13a6084edf1d91be86d15c44b38d7
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-02 05:02:39 +0200

    chore: first commit

--- components/account/src/account/infrastructure/postgres/postgres_memberships_adapter.clj
+++ components/account/src/account/infrastructure/postgres/postgres_memberships_adapter.clj
@@ -1,1 +1,15 @@
-(ns account.infrastructure.postgres.postgres-memberships-adapter)
+(ns account.infrastructure.postgres.postgres-memberships-adapter
+  (:require
+   [postgres-db.interface :as db]
+   [honey.sql :as sql]))
+
+(def table-name :memberships)
+
+(defn insert-membership [data]
+  (-> {:insert-into table-name
+       :values [data]}
+      (sql/format)
+      (db/execute!)))
+
+(comment
+  (insert-membership {}))
--- components/account/src/account/infrastructure/postgres/postgres_organizations_adapter.clj
+++ components/account/src/account/infrastructure/postgres/postgres_organizations_adapter.clj
@@ -1,1 +1,15 @@
-(ns account.infrastructure.postgres.postgres-organizations-adapter)
+(ns account.infrastructure.postgres.postgres-organizations-adapter
+  (:require
+   [postgres-db.interface :as db]
+   [honey.sql :as sql]))
+
+(def table-name :organizations)
+
+(defn insert-organization [data]
+  (-> {:insert-into table-name
+       :values [data]}
+      (sql/format)
+      (db/execute!)))
+
+(comment
+  (insert-organization {:name "hello"}))
