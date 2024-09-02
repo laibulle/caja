5f667e4d7e33c1ade2c5c5279083454fcffcdeef
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-03 01:14:01 +0200

    chore: first commit

---
+++ projects/petitplat-server/dev/user.clj
@@ -0,0 +1,21 @@
+(ns user
+  (:require [juxt.clip.core :as clip]
+            [aero.core :refer [read-config]]
+            [clojure.tools.namespace.repl :refer [refresh]]))
+
+(def system-config (read-config "resources/config.edn"))
+(def system nil)
+
+(defn start []
+  (alter-var-root #'system (constantly (clip/start system-config))))
+
+(defn stop []
+  (alter-var-root #'system
+                  (fn [s] (when s (clip/stop system-config s)))))
+
+(defn reset []
+  (stop)
+  (refresh :after 'dev/go))
+
+(comment
+  (start))
