091182c4565e3525927462adf745807a764e1d41
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-02 05:53:28 +0200

    chore: first commit

--- components/account/src/account/infrastructure/postgres/postgres_organizations_adapter.clj
+++ components/account/src/account/infrastructure/postgres/postgres_organizations_adapter.clj
@@ -1,7 +1,15 @@
 (ns account.infrastructure.postgres.postgres-organizations-adapter
   (:require
    [postgres-db.interface :as db]
-   [honey.sql :as sql]))
+   [honey.sql :as sql]
+   [clojure.set :as set]))
+
+(defn db-to-domain-organization [db-org]
+  (-> db-org
+      (set/rename-keys {:id :id
+                        :name :name
+                        :created_at :created-at
+                        :updated_at :updated-at})))
 
 (def table-name :organizations)
 
--- components/account/src/account/infrastructure/postgres/postgres_users_adapter.clj
+++ components/account/src/account/infrastructure/postgres/postgres_users_adapter.clj
@@ -1,9 +1,21 @@
 (ns account.infrastructure.postgres.postgres-users-adapter
   (:require
    [postgres-db.interface :as db]
-   [honey.sql :as sql]))
+   [honey.sql :as sql]
+   [clojure.set :as set]))
 
 (def table-name :users)
+
+(defn db-to-domain-user [db-user]
+  (-> db-user
+      (set/rename-keys {:users/id :id
+                        :users/name :name
+                        :users/email :email
+                        :users/confirmed_at :confirmed-at
+                        :users/confirmation_token :confirmation-token
+                        :users/created_at :created-at
+                        :users/updated_at :updated-at})))
+
 
 (defn insert-user [data]
   (-> {:insert-into table-name
@@ -12,12 +24,13 @@
       (db/execute!)))
 
 (defn get-user-by-email [email]
-  (-> {:select [:id :name :email :confirmed_at :confirmation-token :created_at :updated_at]
+  (-> {:select [:id :name :email :confirmed_at :confirmation_token :created_at :updated_at]
        :from [table-name]
        :where
        [:= :email email]}
       (sql/format)
-      (db/execute-one!)))
+      (db/execute-one!)
+      (db-to-domain-user)))
 
 (comment
   (insert-user {:name "hello" :email "test@gmail.com"})
