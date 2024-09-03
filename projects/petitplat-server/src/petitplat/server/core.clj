5bd8c96fcc61d8c596779bb219f10f6b9fac35e0
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-03 02:13:25 +0200

    chore: first commit

--- projects/petitplat-server/src/petitplat/server/core.clj
+++ projects/petitplat-server/src/petitplat/server/core.clj
@@ -1,2 +1,9 @@
-(ns petitplat.server.core)
+(ns petitplat.server.core
+  (:require
+   [juxt.clip.core :as clip]
+   [aero.core :refer [read-config]]))
 
+(defn -main
+  [& _]
+  (clip/start (:system-config (read-config (clojure.java.io/resource "config.edn"))))
+  @(promise))
