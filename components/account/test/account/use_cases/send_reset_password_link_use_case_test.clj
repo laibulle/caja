45fba58d5345d19de8079e9ae56c4adcc9bf7020
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-03 16:55:28 +0200

    chore: first commit

---
+++ components/account/test/account/use_cases/send_reset_password_link_use_case_test.clj
@@ -0,0 +1,36 @@
+(ns account.use-cases.send-reset-password-link-use-case-test
+  (:require
+   [clojure.test :refer :all]
+   [account.use-cases.send-reset-password-link-use-case :as use-case]
+   [account.infrastructure.postgres.postgres-users-adapter :as ua]
+   [account.infrastructure.postgres.postgres-password-reset-request-adapter :as ra]
+   [common.interface :refer [=> collect-result]]
+   [messages.interface :as mi]
+   [taoensso.tower :as tower :refer (with-tscope)]
+   [next.jdbc :as jdbc]
+   [postgres-db.interface :as db]
+   [clojure.java.jdbc :as jdbc-mock]))
+
+
+(deftest test-execute
+  (testing "When user is not found"
+    (with-redefs [ua/get-user-by-email (fn [_ _] nil)]
+      (let [result (use-case/execute {:email "nonexistent@example.com"})]
+        (is (= {:errors [:user-not-found]} result)))))
+
+  (testing "When there are too many pending reset requests"
+    (with-redefs [ua/get-user-by-email (fn [_ _] {:id 1 :email "user@example.com"})
+                  ra/list-pending-requests-for-user (fn [_ _] [{:id 1} {:id 2} {:id 3}])]
+      (let [result (use-case/execute {:email "user@example.com"})]
+        (is (= {:errors [:to-many-requests]} result)))))
+
+  (testing "Successful reset password request"
+    (with-redefs [ua/get-user-by-email (fn [_ _] {:id 1 :email "user@example.com"})
+                  ra/list-pending-requests-for-user (fn [_ _] [])
+                  ra/insert-request (fn [_ _] {:id 1 :user-id 1 :token "token12345"})
+                  mi/send-email-from-template (fn [_] :email-sent)]
+      (let [result (use-case/execute {:email "user@example.com"})]
+        (is (= {:request {:id 1 :user-id 1 :token "token12345"}} result))))))
+
+(comment
+  (run-tests))
