33acafd98a73f905f2ede04f00f315f3b94a9e87
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-03 13:53:20 +0200

    chore: first commit

--- components/account/resources/migrations/001-users.up.sql
+++ components/account/resources/migrations/001-users.up.sql
@@ -9,6 +9,14 @@ CREATE TABLE users (
     updated_at TIMESTAMP
 );
 
+CREATE TABLE users_password_reset_requests (
+    id SERIAL PRIMARY KEY,
+    token VARCHAR(100) NOT NULL,
+    user_id INT NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
 CREATE TABLE organizations (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
