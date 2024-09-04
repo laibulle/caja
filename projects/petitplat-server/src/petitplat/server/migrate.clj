7876fe3194c9a6ba4401ef88731603796b9d629c
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-04 23:47:20 +0200

    chore: first commit

--- projects/petitplat-server/src/petitplat/server/migrate.clj
+++ projects/petitplat-server/src/petitplat/server/migrate.clj
@@ -2,20 +2,20 @@
   (:require
    [ragtime.next-jdbc :as jdbc]
    [ragtime.repl :as repl]
-   [clojure.java.io :as io])
-  (:import [java.nio.file Paths Files]))
+   [clojure.java.io :as io]))
+
 
 (defn copy-file [source target]
-  (io/copy (io/file source) (io/file target)))
+  (with-open [in (io/input-stream source)
+              out (io/output-stream target)]
+    (io/copy in out)))
 
 (defn merge-directories [source-dirs target-dir]
   (doseq [dir source-dirs
           file (file-seq (io/file dir))
           :when (.isFile file)]
-    (let [source-path (Paths/get (.getAbsolutePath (io/file dir)) (into-array String []))
-          file-path (Paths/get (.getAbsolutePath file) (into-array String []))
-          relative-path (.relativize source-path file-path)
-          target-file (io/file target-dir (.toString relative-path))]
+    (let [relative-path (.substring (.getAbsolutePath file) (count (.getAbsolutePath (io/file dir))))
+          target-file (io/file target-dir relative-path)]
       (io/make-parents target-file)
       (copy-file file target-file))))
 
