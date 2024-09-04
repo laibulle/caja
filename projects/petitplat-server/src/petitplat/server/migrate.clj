2cc2a785f9d137c9336e6c4779fc68ba6343fbe0
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-04 23:49:09 +0200

    chore: first commit

--- projects/petitplat-server/src/petitplat/server/migrate.clj
+++ projects/petitplat-server/src/petitplat/server/migrate.clj
@@ -5,7 +5,6 @@
    [clojure.java.io :as io])
   (:import [java.nio.file Paths Files]))
 
-
 (defn copy-file [source target]
   (with-open [in (io/input-stream source)
               out (io/output-stream target)]
@@ -15,10 +14,8 @@
   (doseq [dir source-dirs
           file (file-seq (io/file dir))
           :when (.isFile file)]
-    (let [source-path (.toPath (io/file dir))
-          file-path (.toPath file)
-          relative-path (.relativize source-path file-path)
-          target-file (io/file target-dir (.toString relative-path))]
+    (let [relative-path (.relativize (.toPath (io/file dir)) (.toPath file))
+          target-file (io/file target-dir (str relative-path))]
       (io/make-parents target-file)
       (copy-file file target-file))))
 
