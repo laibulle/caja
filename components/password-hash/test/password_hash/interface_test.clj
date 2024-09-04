53f61847a5fb16527385b7da0881f8b00fea1b48
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-04 23:20:24 +0200

    chore: first commit

--- components/password-hash/test/password_hash/interface_test.clj
+++ components/password-hash/test/password_hash/interface_test.clj
@@ -1,6 +1,30 @@
 (ns password-hash.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [password-hash.interface :as password-hash]))
+  (:require [clojure.test :refer :all]
+            [password-hash.interface :refer :all]))
 
-(deftest dummy-test
-  (is (= 1 1)))
+(deftest test-encrypt
+  (testing "Encryption of a password"
+    (let [password "my-secret-password"
+          encrypted-password (encrypt password)]
+      (is (not= password encrypted-password) "Encrypted password should not be the same as the plain password")
+      (is (string? encrypted-password) "Encrypted password should be a string"))))
+
+(deftest test-check
+  (testing "Correct password check"
+    (let [password "my-secret-password"
+          encrypted-password (encrypt password)]
+      (is (true? (check password encrypted-password)) "Check should return true for correct password")))
+
+  (testing "Incorrect password check"
+    (let [password "my-secret-password"
+          encrypted-password (encrypt password)]
+      (is (false? (check "wrong-password" encrypted-password)) "Check should return false for incorrect password"))))
+
+(deftest test-idempotency
+  (testing "Encrypting the same password should not produce the same result"
+    (let [password "my-secret-password"
+          encrypted-password-1 (encrypt password)
+          encrypted-password-2 (encrypt password)]
+      (is (not= encrypted-password-1 encrypted-password-2) "Encrypting the same password twice should produce different results"))))
+
+(run-tests)
