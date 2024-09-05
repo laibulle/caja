c95b9684ca4d9e378e916720ca9fb36586f1092a
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-05 12:17:52 +0200

    chore: first commit

--- experiments/fisher/src/fisher/core.clj
+++ experiments/fisher/src/fisher/core.clj
@@ -1,3 +1,5 @@
-(ns fisher.core)
+(ns fisher.core
+  (:require [bosquet.llm.generator :refer [generate llm]]))
 
-(comment)
+(comment
+  (generate "When I was 6 my sister was half my age. Now I’m 70 how old is my sister?"))
