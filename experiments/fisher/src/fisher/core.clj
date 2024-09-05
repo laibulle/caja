ab856a6099ca83f4c56882bce1d99e47efaa0f17
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-05 13:18:49 +0200

    chore: first commit

--- experiments/fisher/src/fisher/core.clj
+++ experiments/fisher/src/fisher/core.clj
@@ -6,9 +6,5 @@
 (comment
   (generate
    {:question-answer "Question: {{question}}  Answer: {{answer}}"
-    :answer          (llm :ollama k/model-params {:model :llama3.1})
-    :self-eval       ["{{question-answer}}"
-                      ""
-                      "Is this a correct answer? {{test}}"]
-    :test            (llm :ollama k/model-params {:model :llama3.1})}
+    :answer          (llm :ollama k/model-params {:model :llama3.1})}
    {:question "What is the distance from Moon to Io?"}))
