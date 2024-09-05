17893d4cac8d0aeab17b863c13b3c21e1e5293c7
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2024-09-05 23:46:44 +0200

    chore: first commit

--- components/food/src/food/use_cases/create_recipe_use_case.clj
+++ components/food/src/food/use_cases/create_recipe_use_case.clj
@@ -1,7 +1,11 @@
 (ns food.use-cases.create-recipe-use-case
   (:require
    [malli.core :as m]
-   [food.domain.recipe :as d]))
+   [food.domain.recipe :as d]
+   [next.jdbc :as jdbc]
+   [postgres-db.interface :as db]))
+
+(def invalid-input :invalid-input)
 
 (def CreateRecipeInput
   [:map
@@ -11,7 +15,17 @@
 (defn validate-create-recipe-input [input]
   (m/validate CreateRecipeInput input))
 
-(defn execute [input])
+(defn- validate-input [input]
+  (if (validate-create-recipe-input (:data input))
+    input
+    {:errors [invalid-input]}))
+
+(defn execute [input]
+  (jdbc/with-transaction [tx @db/datasource]
+    (-> {:data input :tx tx}
+        (validate-input))))
 
 (comment
-  (validate-create-recipe-input {:member-id 1 :name "Test recipe"}))
+  (def input {:member-id 1 :name "Test recipe"})
+  (validate-create-recipe-input input)
+  (execute input))
